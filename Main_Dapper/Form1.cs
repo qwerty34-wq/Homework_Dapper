3eb2f3e35d55a2530bacdc4ec61e20a5103bc730
qwerty34-qw <wh861440@gmail.com>
2020-11-01 17:17:47 +0200

    Add SellectAll() method Connect to DataBase file

--- Main_Dapper/Form1.cs
+++ Main_Dapper/Form1.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DapperRepository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,9 +14,35 @@ namespace Main_Dapper
 {
     public partial class Form1 : Form
     {
+        static string connection = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+        static GenericUnitOfWork work = new GenericUnitOfWork(connection);
+
+        static IGenericRepository<Product> repoProd = work.Repository<Product>();
+        static IGenericRepository<Category> repoCat = work.Repository<Category>();
+
+        //List<Product> products = repoProd.GetAll().ToList();
+        //List<Category> categories = repoCat.GetAll().ToList();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+        private void SelectAll()
+        {
+            var pc = from p in repoProd.GetAll()
+                     join c in repoCat.GetAll() on p.CategoryId equals c.Id
+                     select new { ProductId = p.Id, ProductName = p.Name, ProductCategory = c.Name };
+
+            pc = pc.ToList();
+
+            dataGridView1.DataSource = pc;
+        }
+
     }
 }
